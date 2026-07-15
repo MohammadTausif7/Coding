93bf145b1c281712634f6baee443326be09c033a
MohammadTausif7 <mohammedtausif64559@gmail.com>
2026-07-14 21:32:15 -0500

    LeetCode Problem 175: Combine Two Tables (Easy)

--- LC0175_CombineTwoTables.sql
+++ LC0175_CombineTwoTables.sql
@@ -4,9 +4,6 @@ p.firstName,
 p.lastName, 
 a.city, 
 a.state 
-FROM 
-person p 
-LEFT JOIN 
-address a 
-ON 
-p.personId = a.personId
+FROM person p 
+LEFT JOIN address a 
+ON p.personId = a.personId
